b4fbcfa1161e238481e0b571e3f8f95a32667451
Fabiano Brito <fabiano.brito@wolt.com>
2022-07-24 17:03:51 +0300

    Proxy api

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/v1/hello": "http://localhost:9000",
+    },
+  }
 })
