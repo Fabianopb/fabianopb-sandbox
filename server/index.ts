d5af89a7d8c1aa8c113da60372bb86324d2c1ed0
Fabiano Brito <fabiano.brito@wolt.com>
2022-07-24 15:41:03 +0300

    Create basic express server

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
+
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+app.get('/api/v1/hello', (_, response) => {
+  response.json('Hello world!');
+});
+
+app.use(express.static(path.resolve('build')));
+
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve('build', 'index.html'));
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = error.statusCode || 500;
+  res.status(statusCode).json({ statusCode, error: error.message });
+});
+
+app.listen(port);
+// eslint-disable-next-line no-console
+console.log(`Server up and running on :${port}`);
