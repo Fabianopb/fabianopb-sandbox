dbe2007f6e696bf873add0ee3fa3b892fcc0a548
Fabiano Brito <fabiano.brito@wolt.com>
2022-07-26 23:12:05 +0300

    Connect to the newly created db before adding extensions

--- scripts/createDb.ts
+++ scripts/createDb.ts
@@ -6,9 +6,12 @@ import { getAppEnv } from '../server/utils';
   const appEnv = getAppEnv();
   const config = knexfile[appEnv];
   const devDbName = config.connection.database;
-  const knex = Knex({ ...config, connection: { ...config.connection, database: undefined } });
 
-  await knex.raw(`CREATE DATABASE ${devDbName}`);
+  const initialKnex = Knex({ ...config, connection: { ...config.connection, database: undefined } });
+  await initialKnex.raw(`CREATE DATABASE ${devDbName}`);
+  await initialKnex.destroy();
+  
+  const knex = Knex(config);
   await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   await knex.destroy();
 })();
