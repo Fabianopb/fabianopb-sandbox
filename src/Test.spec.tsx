d8cf78df65ef298f037bc180a2df15da19563e4f
Fabiano Brito <fabiano.brito@wolt.com>
2022-08-16 21:42:46 +0300

    Remove legacy components

--- src/Test.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Test from './Test';
-
-describe('<Test />', () => {
-  test('test renders test', () => {
-    render(<Test />);
-    expect(screen.getByText('This is a test for a test')).toBeInTheDocument();
-  });
-});
--- src/Test.tsx
+++
@@ -1,3 +0,0 @@
-const Test = () => <div>This is a test for a test</div>;
-
-export default Test;
