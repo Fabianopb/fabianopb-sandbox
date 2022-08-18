87476e2dc13ed6f3df41d9b2ac00dfe7f1e69a04
Fabiano Brito <fabiano.brito@wolt.com>
2022-08-18 22:43:54 +0300

    Create basic layout and style the topbar

--- src/portfolio/index.tsx
+++ src/portfolio/index.tsx
@@ -1,3 +1,67 @@
-const PortfolioView = () => <div>Portfolio view</div>;
+import styled, { css } from 'styled-components';
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Topbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  padding: 0 32px;
+  background-color: #17293a;
+  color: #fff;
+`;
+
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: 400;
+`;
+
+const Navigation = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const linkStyle = css`
+  color: #fff;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+  &:active {
+    color: #d9d0e3;
+  }
+`;
+
+const ContactLink = styled.div`
+  ${linkStyle}
+  margin-right: 24px;
+`;
+
+const BlogLink = styled.a`
+  ${linkStyle}
+`;
+
+const PortfolioView = () => (
+  <MainWrapper>
+    <Topbar>
+      <Logo>Fabiano Brito</Logo>
+      <Navigation>
+        <ContactLink>Contact</ContactLink>
+        <BlogLink href="https://medium.com/@fabianopb" target="_blank" rel="noopener noreferrer">
+          Blog
+        </BlogLink>
+      </Navigation>
+    </Topbar>
+    <div>Banner</div>
+    <div>About me</div>
+    <div>Skills</div>
+    <div>Selected Work</div>
+    <div>Footer</div>
+  </MainWrapper>
+);
 
 export default PortfolioView;
