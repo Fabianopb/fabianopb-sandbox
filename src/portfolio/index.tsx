ada3aa7208b34cbb2d3fd3374ba29f79cf25ad86
Fabiano Brito <fabiano.brito@wolt.com>
2022-08-22 22:33:11 +0300

    Add purely styled skills section

--- src/portfolio/index.tsx
+++ src/portfolio/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Divider, Link } from '@mui/material';
+import { useState } from 'react';
 import styled from 'styled-components';
 import bannerImageSrc from '../assets/banner.jpeg';
 import profileImageSrc from '../assets/fabiano.jpeg';
@@ -146,73 +147,154 @@ const ProfileParagraph = styled.p`
   color: #555;
 `;
 
-const PortfolioView = () => (
-  <MainWrapper>
-    <Topbar>
-      <Logo>Fabiano Brito</Logo>
-      <Navigation>
-        <StyledLink href="#" underline="hover">
-          Contact
-        </StyledLink>
-        <StyledLink href="https://medium.com/@fabianopb" target="_blank" rel="noopener noreferrer" underline="hover">
-          Blog
-        </StyledLink>
-      </Navigation>
-    </Topbar>
-    <BannerContainer>
-      <ImageOverlay>
-        <BannerImage src={bannerImageSrc} />
-      </ImageOverlay>
+const SkillsContainer = styled.div`
+  width: 600px;
+  margin: auto;
+`;
 
-      <BannerText>
-        <BannerTitle>Hey, I&apos;m Fabiano!</BannerTitle>
-        <BannerBody>
-          Software Engineer and Business Designer based in Helsinki. I develop real-time and responsive web-based apps.
-          Some of my preferred tools at the moment are React, TypeScript, D3, Sass, Node, Express, Mongo, Gulp, Webpack.
-          <br />
-          <br />I have also side projects in RoR and native Android.
-        </BannerBody>
-        <BannerButton variant="outlined" size="large">
-          Show me
-        </BannerButton>
-      </BannerText>
-    </BannerContainer>
+const SkillsHeader = styled.div`
+  display: flex;
+  font-weight: bold;
+`;
 
-    <Section>
-      <SectionTitle>About me</SectionTitle>
-      <AboutMeHeader>
-        <ProfilePicture src={profileImageSrc} />
-        <div>
-          <ProfileMotto>
-            In order to have exciting experiences, to see different places, and to meet amazing people, you have to try
-            and fail a lot.
-          </ProfileMotto>
-          <ProfileQuote>The journey is the destination.</ProfileQuote>
-        </div>
-      </AboutMeHeader>
-      <ProfileParagraph>
-        After graduating in Engineering in 2011, I started working with industrial facilities design, and after a couple
-        of years, seeking for new challenges, I got involved with design-based approaches for innovation, start-ups and
-        entrepreneurship. That&apos;s how I&apos;ve decided to get out of my comfort zone and ended up in Finland, in a
-        Master Program of Business Design.
-      </ProfileParagraph>
-      <ProfileParagraph>
-        During these past few years I got involved in mind-blowing experiences such as a business design case for water
-        purification in Tanzania, a start-up that teaches kids computational thinking, ended up in a Hackathon in Tokyo,
-        and spent a few months in Spain to start building a software team during a business expansion.
-      </ProfileParagraph>
-    </Section>
+const SkillLevel = styled.div`
+  flex: 1;
+  text-align: center;
+  font-size: 12px;
+  color: #555;
+  &:not(:last-child) {
+    border-right: 1px solid #555;
+  }
+`;
 
-    <StyledDivider variant="middle" />
+const SkillBar = styled.div<{ value: number }>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 12px;
+  padding: 0 8px;
+  width: 100%;
+  background-color: #d1d0d7;
+  height: 24px;
+  border-radius: 4px;
+  ::after {
+    position: absolute;
+    display: block;
+    content: '';
+    background-color: #17293a;
+    height: 100%;
+    width: ${({ value }) => `${value}%`};
+    left: 0;
+    top: 0;
+    border-radius: 4px;
+    transition: width 1000ms ease-in-out;
+  }
+`;
 
-    <Section>
-      <SectionTitle>Skills</SectionTitle>
-    </Section>
+const SkillBarValue = styled.div`
+  position: relative;
+  color: #fff;
+  z-index: 1;
+`;
 
-    <div>Skills</div>
-    <div>Selected Work</div>
-    <div>Footer</div>
-  </MainWrapper>
-);
+const skills = [
+  { name: 'Problem solving', value: 100 },
+  { name: 'Programming', value: 95 },
+  { name: 'Innovation management', value: 90 },
+  { name: 'Project management', value: 85 },
+  { name: 'Marketing', value: 80 },
+  { name: 'Facilitation', value: 75 },
+  { name: 'Business strategy', value: 65 },
+  { name: 'UI/UX design', value: 60 },
+  { name: 'Data analytics', value: 55 },
+  { name: 'Adobe Ps, Ai & In', value: 40 },
+];
+
+const PortfolioView = () => {
+  const [hasHoveredSkills, setHasHoveredSkills] = useState(false);
+
+  return (
+    <MainWrapper>
+      <Topbar>
+        <Logo>Fabiano Brito</Logo>
+        <Navigation>
+          <StyledLink href="#" underline="hover">
+            Contact
+          </StyledLink>
+          <StyledLink href="https://medium.com/@fabianopb" target="_blank" rel="noopener noreferrer" underline="hover">
+            Blog
+          </StyledLink>
+        </Navigation>
+      </Topbar>
+      <BannerContainer>
+        <ImageOverlay>
+          <BannerImage src={bannerImageSrc} />
+        </ImageOverlay>
+
+        <BannerText>
+          <BannerTitle>Hey, I&apos;m Fabiano!</BannerTitle>
+          <BannerBody>
+            Software Engineer and Business Designer based in Helsinki. I develop real-time and responsive web-based
+            apps. Some of my preferred tools at the moment are React, TypeScript, D3, Sass, Node, Express, Mongo, Gulp,
+            Webpack.
+            <br />
+            <br />I have also side projects in RoR and native Android.
+          </BannerBody>
+          <BannerButton variant="outlined" size="large">
+            Show me
+          </BannerButton>
+        </BannerText>
+      </BannerContainer>
+
+      <Section>
+        <SectionTitle>About me</SectionTitle>
+        <AboutMeHeader>
+          <ProfilePicture src={profileImageSrc} />
+          <div>
+            <ProfileMotto>
+              In order to have exciting experiences, to see different places, and to meet amazing people, you have to
+              try and fail a lot.
+            </ProfileMotto>
+            <ProfileQuote>The journey is the destination.</ProfileQuote>
+          </div>
+        </AboutMeHeader>
+        <ProfileParagraph>
+          After graduating in Engineering in 2011, I started working with industrial facilities design, and after a
+          couple of years, seeking for new challenges, I got involved with design-based approaches for innovation,
+          start-ups and entrepreneurship. That&apos;s how I&apos;ve decided to get out of my comfort zone and ended up
+          in Finland, in a Master Program of Business Design.
+        </ProfileParagraph>
+        <ProfileParagraph>
+          During these past few years I got involved in mind-blowing experiences such as a business design case for
+          water purification in Tanzania, a start-up that teaches kids computational thinking, ended up in a Hackathon
+          in Tokyo, and spent a few months in Spain to start building a software team during a business expansion.
+        </ProfileParagraph>
+      </Section>
+
+      <StyledDivider variant="middle" />
+
+      <Section>
+        <SectionTitle>Skills</SectionTitle>
+        <SkillsContainer onMouseOver={() => (!hasHoveredSkills ? setHasHoveredSkills(true) : undefined)}>
+          <SkillsHeader>
+            <SkillLevel>BEGINNER</SkillLevel>
+            <SkillLevel>PROFICIENT</SkillLevel>
+            <SkillLevel>EXPERT</SkillLevel>
+          </SkillsHeader>
+          {skills.map((skill) => (
+            <SkillBar key={skill.name} value={hasHoveredSkills ? skill.value : 0}>
+              <SkillBarValue>{skill.name}</SkillBarValue>
+              <SkillBarValue>{skill.value}%</SkillBarValue>
+            </SkillBar>
+          ))}
+        </SkillsContainer>
+      </Section>
+
+      <div>Selected Work</div>
+      <div>Footer</div>
+    </MainWrapper>
+  );
+};
 
 export default PortfolioView;
