4e27ebefd6f8025b332e4dd32cedb5e27ee2b9dd
Fabiano Brito <fabiano.brito@wolt.com>
2022-08-27 15:41:07 +0300

    Create simple test for the portfolio index

---
+++ src/index.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import PortfolioView from './portfolio';
+
+jest.mock('recharts', () => {
+  const OriginalRechartsModule = jest.requireActual('recharts');
+  return {
+    ...OriginalRechartsModule,
+    ResponsiveContainer: ({ height, children }: { height: number; children?: ReactNode }) => (
+      <div className="recharts-responsive-container" style={{ width: 800, height }}>
+        {children}
+      </div>
+    ),
+  };
+});
+
+const wrapper = ({ children }: { children?: ReactNode }) => <MemoryRouter>{children}</MemoryRouter>;
+
+describe('PortfolioView()', () => {
+  it('renders the index', () => {
+    const { getByText } = render(<PortfolioView />, { wrapper });
+    expect(getByText('Fabiano Brito')).toBeInTheDocument();
+  });
+});
