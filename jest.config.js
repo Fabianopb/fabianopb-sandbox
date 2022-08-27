28e4c4725d0629a9a3196c926f55b0d05949c148
Fabiano Brito <fabiano.brito@wolt.com>
2022-08-27 15:40:38 +0300

    Map assets and d3 for tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/src/mocks/fileMock.ts',
+    '^d3-(.*)$': `<rootDir>/node_modules/d3-$1/dist/d3-$1`,
+  },
 };
