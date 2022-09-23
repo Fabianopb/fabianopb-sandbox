7b1976bb7b05ad62f4bf07d10d309d80bc399c7f
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-23 15:41:10 +0300

    Use constant collection name

--- server/portfolio/skills.ts
+++ server/portfolio/skills.ts
@@ -1,16 +1,12 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 import { database } from '../database';
-
-type Skill = {
-  name: string;
-  value: number;
-};
+import { PORTFOLIO_SKILLS } from './collections';
 
 const skills = Router();
 
 skills.route('/skills').get(async (_, response) => {
-  const collection = database.collection<Skill>('skills');
+  const collection = database.collection(PORTFOLIO_SKILLS);
   const cursor = collection.find();
   const skills = await cursor.toArray();
   return response.status(200).json(skills);
@@ -18,7 +14,7 @@ skills.route('/skills').get(async (_, response) => {
 
 skills.route('/skill').post(bodyParser.json(), async (request, response) => {
   try {
-    const collection = database.collection<Skill>('skills');
+    const collection = database.collection(PORTFOLIO_SKILLS);
     const skill = request.body;
     await collection.insertOne(skill);
     return response.status(200).json('Item saved!');
