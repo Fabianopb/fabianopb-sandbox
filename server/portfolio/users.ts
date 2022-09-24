52a0311ef8f88ec9b7003dd0fed2f9e587d0cf81
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-24 12:33:35 +0300

    Handle user authentication with passport

--- server/portfolio/users.ts
+++ server/portfolio/users.ts
@@ -1,29 +1,34 @@
+import bcrypt from 'bcryptjs';
 import { Router } from 'express';
 import { database } from '../database';
 import { PORTFOLIO_USERS } from './collections';
-import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
 
 type User = {
   username: string;
   password: string;
 };
 
+passport.use(
+  new LocalStrategy(async (username, password, cb) => {
+    try {
+      const collection = database.collection<User>(PORTFOLIO_USERS);
+      const user = await collection.findOne({ username });
+      const isValidUser = user && bcrypt.compareSync(password, user.password);
+      if (!isValidUser) {
+        return cb(null, false, { message: 'Invalid username or password' });
+      }
+      return cb(null, user);
+    } catch (error) {
+      return cb(error);
+    }
+  })
+);
+
 const usersRouter = Router();
 
-usersRouter.route('/users/login').post(async (request, response) => {
-  try {
-    const collection = database.collection<User>(PORTFOLIO_USERS);
-    const { username, password } = request.body;
-    const user = await collection.findOne({ username });
-    const isValidUser = user && bcrypt.compareSync(password, user.password);
-    if (!isValidUser) {
-      return response.status(403).send({ error: 'Invalid username or password' });
-    }
-    return response.status(200).json('Return token');
-  } catch (error) {
-    return response.status(400).send({ error });
-  }
-});
+usersRouter.route('/users/login').post(passport.authenticate('local'));
 
 usersRouter.route('/users/register-admin').post(async (request, response) => {
   try {
