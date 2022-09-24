95f8faa7b2de25bfb199a7c35c087fe90485702a
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-24 18:23:09 +0300

    Add put route for skills

--- server/portfolio/skills.ts
+++ server/portfolio/skills.ts
@@ -1,24 +1,47 @@
 import { Router } from 'express';
+import { ObjectId } from 'mongodb';
 import { database } from '../database';
+import auth from './auth';
 import { PORTFOLIO_SKILLS } from './collections';
+
+type Skill = {
+  name: string;
+  value: number;
+};
 
 const skillsRouter = Router();
 
-skillsRouter.route('/skills').get(async (_, response) => {
+skillsRouter.get('/skills', async (_, response) => {
   const collection = database.collection(PORTFOLIO_SKILLS);
   const cursor = collection.find();
   const skills = await cursor.toArray();
   return response.status(200).json(skills);
 });
 
-skillsRouter.route('/skill').post(async (request, response) => {
+skillsRouter.post('/skills', auth, async (request, response) => {
   try {
     const collection = database.collection(PORTFOLIO_SKILLS);
+    const skills = request.body;
+    await collection.insertMany(skills);
+    return response.status(200).json('Items saved!');
+  } catch (error: any) {
+    return response.status(400).send(error.message);
+  }
+});
+
+skillsRouter.put('/skills/:skillId', auth, async (request, response) => {
+  try {
+    const { skillId } = request.params;
     const skill = request.body;
-    await collection.insertOne(skill);
-    return response.status(200).json('Item saved!');
-  } catch (error) {
-    return response.status(400).send({ error });
+    const replaceDocument: Skill = { name: skill.name, value: skill.value };
+    const collection = database.collection(PORTFOLIO_SKILLS);
+    const result = await collection.replaceOne({ _id: new ObjectId(skillId) }, replaceDocument);
+    if (result.matchedCount === 0) {
+      return response.status(404).send('Item not found');
+    }
+    return response.status(200).json({ message: 'Item updated' });
+  } catch (error: any) {
+    return response.status(400).send(error.message);
   }
 });
 
