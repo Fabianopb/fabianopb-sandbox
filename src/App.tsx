5e2413176d34b93e4d7f5cce23e55c8647cd4084
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 14:33:35 +0300

    Remove example data fetching from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { getSkills } from './api';
 import PortfolioView from './portfolio';
 import Layout from './portfolio/components/Layout';
 import ProjectDetails from './portfolio/components/ProjectDetails';
 
 const App = () => {
-  const { data } = useQuery(['portfolio', 'skills'], getSkills);
-  console.log(data);
   return (
     <BrowserRouter>
       <Routes>
