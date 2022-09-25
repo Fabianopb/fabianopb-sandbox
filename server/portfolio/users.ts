6f9aa29e9dc518dbf50b9fa2fe7a961f069b8e82
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 14:06:31 +0300

    Use a better error handling for user endpoints

--- server/portfolio/users.ts
+++ server/portfolio/users.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { database } from '../database';
 import { PORTFOLIO_USERS } from './collections';
 import { generateJwt } from './auth';
+import { BadRequestError, UnauthorizedError } from '../utils';
 
 type User = {
   _id?: string;
@@ -12,36 +13,36 @@ type User = {
 
 const usersRouter = Router();
 
-usersRouter.route('/users/login').post(async (request, response) => {
+usersRouter.route('/users/login').post(async (req, res, next) => {
   try {
     const collection = database.collection<User>(PORTFOLIO_USERS);
-    const { username, password } = request.body;
+    const { username, password } = req.body;
     const user = await collection.findOne({ username });
     const isValidUser = user && bcrypt.compareSync(password, user.password);
     if (!isValidUser) {
-      return response.status(403).send({ error: 'Invalid username or password' });
+      throw new UnauthorizedError('Invalid username or password provided');
     }
     const token = generateJwt(user);
-    return response.status(200).json({ token });
+    return res.status(200).json({ token });
   } catch (error) {
-    return response.status(400).send({ error });
+    next(error);
   }
 });
 
-usersRouter.route('/users/register-admin').post(async (request, response) => {
+usersRouter.route('/users/register-admin').post(async (req, res, next) => {
   try {
     const collection = database.collection<User>(PORTFOLIO_USERS);
     const existingDocuments = await collection.countDocuments();
     if (existingDocuments > 0) {
-      return response.status(400).send({ error: 'Admin user already exists!' });
+      throw new BadRequestError('Admin user already exists');
     }
-    const { username, password } = request.body;
+    const { username, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
     await collection.insertOne({ username, password: hashPassword });
-    return response.status(200).json('Admin user created!');
+    return res.status(200).json('Admin user created!');
   } catch (error) {
-    return response.status(400).send({ error });
+    next(error);
   }
 });
 
