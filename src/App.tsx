957ac8ce43035344c20790d5ea72ab987497d445
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 14:58:35 +0300

    Install and configure use-query-params

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import PortfolioView from './portfolio';
 import Layout from './portfolio/components/Layout';
@@ -6,14 +8,16 @@ import ProjectDetails from './portfolio/components/ProjectDetails';
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/portfolio" replace />} />
-        <Route path="/portfolio" element={<Layout />}>
-          <Route index element={<PortfolioView />} />
-          <Route path="projects/:id" element={<ProjectDetails />} />
-        </Route>
-        <Route path="*" element={<h2>Nothing to see here... :/</h2>} />
-      </Routes>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/portfolio" replace />} />
+          <Route path="/portfolio" element={<Layout />}>
+            <Route index element={<PortfolioView />} />
+            <Route path="projects/:id" element={<ProjectDetails />} />
+          </Route>
+          <Route path="*" element={<h2>Nothing to see here... :/</h2>} />
+        </Routes>
+      </QueryParamProvider>
     </BrowserRouter>
   );
 };
