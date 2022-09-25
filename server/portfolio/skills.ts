a947c17e6ae88186f157570aeaa85b8a51c72d6f
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 13:55:55 +0300

    Return instance of not found error

--- server/portfolio/skills.ts
+++ server/portfolio/skills.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { ObjectId } from 'mongodb';
 import { database } from '../database';
+import { NotFoundError } from '../utils';
 import auth from './auth';
 import { PORTFOLIO_SKILLS } from './collections';
 
@@ -11,53 +12,53 @@ type Skill = {
 
 const skillsRouter = Router();
 
-skillsRouter.get('/skills', async (_, response, next) => {
+skillsRouter.get('/skills', async (_, res, next) => {
   try {
     const collection = database.collection(PORTFOLIO_SKILLS);
     const cursor = collection.find();
     const skills = await cursor.toArray();
-    return response.status(200).json(skills);
+    return res.status(200).json(skills);
   } catch (error) {
     next(error);
   }
 });
 
-skillsRouter.post('/skills', auth, async (request, response, next) => {
+skillsRouter.post('/skills', auth, async (req, res, next) => {
   try {
     const collection = database.collection(PORTFOLIO_SKILLS);
-    const skills = request.body;
+    const skills = req.body;
     await collection.insertMany(skills);
-    return response.status(200).json('Items created');
+    return res.status(200).json('Items created');
   } catch (error) {
     next(error);
   }
 });
 
-skillsRouter.put('/skills/:skillId', auth, async (request, response, next) => {
+skillsRouter.put('/skills/:skillId', auth, async (req, res, next) => {
   try {
-    const { skillId } = request.params;
-    const skill = request.body;
+    const { skillId } = req.params;
+    const skill = req.body;
     const replaceDocument: Skill = { name: skill.name, value: skill.value };
     const collection = database.collection(PORTFOLIO_SKILLS);
     const result = await collection.replaceOne({ _id: new ObjectId(skillId) }, replaceDocument);
     if (result.matchedCount === 0) {
-      return response.status(404).send('Item not found');
+      throw new NotFoundError(`Item '${skillId}' not found`);
     }
-    return response.status(200).json({ message: 'Item updated' });
+    return res.status(200).json({ message: 'Item updated' });
   } catch (error) {
     next(error);
   }
 });
 
-skillsRouter.delete('/skills/:skillId', auth, async (request, response, next) => {
+skillsRouter.delete('/skills/:skillId', auth, async (req, res, next) => {
   try {
-    const { skillId } = request.params;
+    const { skillId } = req.params;
     const collection = database.collection(PORTFOLIO_SKILLS);
     const result = await collection.deleteOne({ _id: new ObjectId(skillId) });
     if (result.deletedCount === 0) {
-      return response.status(404).send('Item not found');
+      throw new NotFoundError(`Item '${skillId}' not found`);
     }
-    return response.status(200).json({ message: 'Item deleted' });
+    return res.status(200).json({ message: 'Item deleted' });
   } catch (error) {
     next(error);
   }
