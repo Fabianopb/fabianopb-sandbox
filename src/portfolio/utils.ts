b6ceaccac30ffdbcd0c5fb614397dd303a177531
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 15:46:34 +0300

    Add session management utils

---
+++ src/portfolio/utils.ts
@@ -0,0 +1,23 @@
+import jwtDecode from 'jwt-decode';
+
+export const setSession = (token: string) => {
+  localStorage.setItem('portfolio_token', token);
+};
+
+export const clearSession = () => {
+  localStorage.removeItem('portfolio_token');
+};
+
+export const isSessionValid = () => {
+  const token = localStorage.getItem('portfolio_token');
+  if (!token) {
+    return false;
+  }
+  const decoded = jwtDecode<{ exp: number }>(token);
+  const now = new Date();
+  if (now.valueOf() > decoded.exp * 1000) {
+    clearSession();
+    return false;
+  }
+  return true;
+};
