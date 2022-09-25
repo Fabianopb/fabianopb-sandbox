60b1dea9021839002c35dd7b298aa486ad807190
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-25 15:18:06 +0300

    Improve dialog design

--- src/portfolio/components/LoginDialog.tsx
+++ src/portfolio/components/LoginDialog.tsx
@@ -1,21 +1,35 @@
 import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
+import styled from 'styled-components';
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
 };
 
+const StyledDialogActions = styled(DialogActions)`
+  padding: 8px 24px 24px;
+`;
+
+const StyledTitle = styled(DialogTitle)`
+  color: #8e8f98;
+`;
+
+const StyledInput = styled(TextField)`
+  margin-top: 16px;
+`;
+
 const LoginDialog = ({ isOpen, onClose }: Props) => (
   <Dialog open={isOpen} onClose={onClose}>
-    <DialogTitle>Login</DialogTitle>
+    <StyledTitle>Login</StyledTitle>
     <DialogContent>
-      <TextField label="Username" fullWidth variant="outlined" size="small" />
-      <TextField label="Password" type="password" fullWidth variant="outlined" size="small" />
+      <StyledInput label="Username" fullWidth variant="outlined" size="small" />
+      <StyledInput label="Password" type="password" fullWidth variant="outlined" size="small" />
     </DialogContent>
-    <DialogActions>
-      <Button onClick={onClose}>Cancel</Button>
-      <Button onClick={onClose}>Confirm</Button>
-    </DialogActions>
+    <StyledDialogActions>
+      <Button variant="outlined" onClick={onClose}>
+        Confirm
+      </Button>
+    </StyledDialogActions>
   </Dialog>
 );
 
