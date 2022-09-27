eed2ad17c3727a62d6f0c28ce2646668e2e36663
Fabiano Brito <fabiano.brito@wolt.com>
2022-09-27 09:41:52 +0300

    Add auth interceptor. Fix delete endpoint call.

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,16 @@
 import axios from 'axios';
+import { getToken } from './portfolio/utils';
 
 const agent = axios.create({ baseURL: '/api/v1/portfolio' });
+
+agent.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token && config.headers) {
+    // eslint-disable-next-line dot-notation
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
 
 type Skill = {
   _id: string;
@@ -23,4 +33,4 @@ export const addSkills = (payload: SkillPayload[]) => agent.post('/skills', payl
 export const editSkill = (skillId: string, payload: SkillPayload) =>
   agent.put(`/skills/${skillId}`, payload).then((res) => res.data);
 
-export const deleteSkill = (skillId: string) => agent.put(`/skills/${skillId}`).then((res) => res.data);
+export const deleteSkill = (skillId: string) => agent.delete(`/skills/${skillId}`).then((res) => res.data);
