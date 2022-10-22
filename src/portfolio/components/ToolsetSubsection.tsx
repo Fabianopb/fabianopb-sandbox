00c8da462e1c07d0b907b0c8098ee4e560062aa1
Fabiano Brito <fabiano.brito@wolt.com>
2022-10-22 19:14:52 +0300

    Fix toolset subsection for mobile

--- src/portfolio/components/ToolsetSubsection.tsx
+++ src/portfolio/components/ToolsetSubsection.tsx
@@ -21,6 +21,10 @@ type Props = {
 const ToolsetContainer = styled.div`
   width: 600px;
   margin: auto;
+  @media (max-width: 768px) {
+    width: initial;
+    margin: 12px 8px;
+  }
 `;
 
 const Header = styled.div`
