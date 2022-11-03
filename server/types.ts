d43b0d6d943520000cbe63aff23e2fbdd7137ee9
Fabiano Brito <fabiano.brito@wolt.com>
2022-11-03 21:28:20 +0200

    Add ps store hash to user types

--- server/types.ts
+++ server/types.ts
@@ -1,8 +1,9 @@
-export type Role = 'portfolio_admin';
+export type Role = 'portfolio_admin' | 'playstation_user';
 
 export type User = {
   _id?: string;
   username: string;
   password: string;
   role: Role;
+  psStoreHash?: string;
 };
