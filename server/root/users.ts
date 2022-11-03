40aa579b5fdfbc9444ec57b85c2ef76ec287f8b5
Fabiano Brito <fabiano.brito@wolt.com>
2022-11-03 21:28:49 +0200

    Validate user creation

--- server/root/users.ts
+++ server/root/users.ts
@@ -24,18 +24,32 @@ usersRouter.route('/users/login').post(async (req, res, next) => {
   }
 });
 
-usersRouter.route('/users/register-admin').post(async (req, res, next) => {
+usersRouter.route('/users/register').post(async (req, res, next) => {
   try {
+    const { username, password, role, psStoreHash } = req.body as User;
     const collection = database.collection<User>(USERS);
-    const existingDocuments = await collection.countDocuments();
-    if (existingDocuments > 0) {
-      throw new BadRequestError('Admin user already exists');
+
+    if (role === 'portfolio_admin') {
+      const cursor = await collection.find({ role });
+      const adminUsers = await cursor.toArray();
+      if (adminUsers.length > 0) {
+        throw new BadRequestError('Cannot create another admin user');
+      }
     }
-    const { username, password } = req.body;
+
+    const user = await collection.findOne({ username });
+    if (user) {
+      throw new BadRequestError(`User "${username}" already exists`);
+    }
+
+    if (role === 'playstation_user' && !psStoreHash) {
+      throw new BadRequestError("'psStoreHash' is required for 'playstation_user' role");
+    }
+
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
-    await collection.insertOne({ username, password: hashPassword, role: 'portfolio_admin' });
-    return res.status(200).json('Admin user created!');
+    await collection.insertOne({ username, password: hashPassword, role, psStoreHash });
+    return res.status(200).json('User created!');
   } catch (error) {
     next(error);
   }
