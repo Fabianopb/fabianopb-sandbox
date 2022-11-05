81569a7b16dc7cdee64970772bbe1f117d4601a0
Fabiano Brito <fabiano.brito@wolt.com>
2022-11-05 12:28:22 +0200

    Add userId to wishlist schema

--- server/playstation/schemas.ts
+++ server/playstation/schemas.ts
@@ -1,9 +1,12 @@
 export const playstationWishlistSchema = {
   $jsonSchema: {
     bsonType: 'object',
-    required: ['gameId'],
+    required: ['gameId', 'userId'],
     properties: {
       gameId: {
+        bsonType: 'string',
+      },
+      userId: {
         bsonType: 'string',
       },
       imageSrc: {
