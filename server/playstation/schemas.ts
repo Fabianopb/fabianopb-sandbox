d933fed3f8773f7f462fa5d1365fee16e47c5174
Fabiano Brito <fabiano.brito@wolt.com>
2022-11-06 17:58:13 +0200

    Add product data and name to schema

--- server/playstation/schemas.ts
+++ server/playstation/schemas.ts
@@ -1,13 +1,19 @@
 export const playstationWishlistSchema = {
   $jsonSchema: {
     bsonType: 'object',
-    required: ['gameId', 'userId'],
+    required: ['gameId', 'userId', 'name', 'data'],
     properties: {
       gameId: {
         bsonType: 'string',
       },
       userId: {
         bsonType: 'string',
+      },
+      name: {
+        bsonType: 'string',
+      },
+      data: {
+        bsonType: 'object',
       },
       imageSrc: {
         bsonType: ['null', 'string'],
