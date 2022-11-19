242db8db38cc42feb4d5dc8e51264a3a5fb0eb24
Fabiano Brito <fabiano.brito@wolt.com>
2022-11-19 11:21:52 +0200

    Remove timezone from cron job due to memory leak issue

--- server/playstation/cronJobs.ts
+++ server/playstation/cronJobs.ts
@@ -9,65 +9,59 @@ import { PLAYSTATION_WISHLIST } from './collections';
 import { agent, getPsStoreRequestParams } from './utils';
 
 export const init = () => {
-  cron.schedule(
-    '5 2 * * *',
-    async () => {
-      const now = new Date();
-      console.log(`Updating all wishlist items at ${now.toLocaleString()}`);
+  // Runs 1:05 CET, which is 2:05 EET. Cannot set timezone due to memory leak in node-cron
+  cron.schedule('5 1 * * *', async () => {
+    const now = new Date();
+    console.log(`Updating all wishlist items at ${now.toLocaleString()}`);
 
-      const wishlistCol = database.collection<Omit<WishlistItem, '_id'>>(PLAYSTATION_WISHLIST);
-      const wishlistCursor = wishlistCol.find({
-        'data.productRetrieve.webctas': {
-          $elemMatch: {
-            type: 'ADD_TO_CART',
-            $or: [
-              {
-                'price.endTime': null,
-              },
-              {
-                'price.endTime': { $lt: now.valueOf().toString() },
-              },
-            ],
-          },
+    const wishlistCol = database.collection<Omit<WishlistItem, '_id'>>(PLAYSTATION_WISHLIST);
+    const wishlistCursor = wishlistCol.find({
+      'data.productRetrieve.webctas': {
+        $elemMatch: {
+          type: 'ADD_TO_CART',
+          $or: [
+            {
+              'price.endTime': null,
+            },
+            {
+              'price.endTime': { $lt: now.valueOf().toString() },
+            },
+          ],
         },
-      });
-      const wishlistItems = await wishlistCursor.toArray();
+      },
+    });
+    const wishlistItems = await wishlistCursor.toArray();
 
-      const usersCol = database.collection<User>(USERS);
-      const usersCursor = usersCol.find({ roles: 'playstation_user' });
-      const users = await usersCursor.toArray();
+    const usersCol = database.collection<User>(USERS);
+    const usersCursor = usersCol.find({ roles: 'playstation_user' });
+    const users = await usersCursor.toArray();
 
-      const wishlistItemsPerUser = users.map((user) => ({
-        user,
-        items: wishlistItems.filter((item) => new ObjectId(item.userId).equals(user._id)),
-      }));
+    const wishlistItemsPerUser = users.map((user) => ({
+      user,
+      items: wishlistItems.filter((item) => new ObjectId(item.userId).equals(user._id)),
+    }));
 
-      for (const { user, items } of wishlistItemsPerUser) {
-        console.log(`Updating ${items.length} items for user ${user._id}...`);
-        for (const item of items) {
-          if (user.psStoreHash) {
-            const requestParams = getPsStoreRequestParams(item.gameId, user.psStoreHash);
-            const resource = await agent.get('', { params: requestParams });
-            const replaceDocument = {
-              ...item,
-              data: resource.data.data,
-              updatedAt: new Date().valueOf(),
-            };
-            const result = await wishlistCol.replaceOne({ _id: new ObjectId(replaceDocument._id) }, replaceDocument);
-            if (result.matchedCount === 0) {
-              console.log(`Error: Wishlist item '${replaceDocument._id}' not found!`);
-            } else {
-              console.log(`Update successful for '${replaceDocument._id}'!`);
-            }
+    for (const { user, items } of wishlistItemsPerUser) {
+      console.log(`Updating ${items.length} items for user ${user._id}...`);
+      for (const item of items) {
+        if (user.psStoreHash) {
+          const requestParams = getPsStoreRequestParams(item.gameId, user.psStoreHash);
+          const resource = await agent.get('', { params: requestParams });
+          const replaceDocument = {
+            ...item,
+            data: resource.data.data,
+            updatedAt: new Date().valueOf(),
+          };
+          const result = await wishlistCol.replaceOne({ _id: new ObjectId(replaceDocument._id) }, replaceDocument);
+          if (result.matchedCount === 0) {
+            console.log(`Error: Wishlist item '${replaceDocument._id}' not found!`);
           } else {
-            console.log(`Error while updating user '${user._id}' wishlist! No PS Store Hash found!`);
+            console.log(`Update successful for '${replaceDocument._id}'!`);
           }
+        } else {
+          console.log(`Error while updating user '${user._id}' wishlist! No PS Store Hash found!`);
         }
       }
-    },
-    {
-      scheduled: true,
-      timezone: 'Europe/Helsinki',
     }
-  );
+  });
 };
