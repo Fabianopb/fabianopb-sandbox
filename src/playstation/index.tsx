539ed8d0e3e668590eb11d12578cf30416a7f71f
Fabiano Brito <fabiano.brito@wolt.com>
2023-03-15 21:05:27 +0200

    Create hardcoded PS3 wishlist

--- src/playstation/index.tsx
+++ src/playstation/index.tsx
@@ -131,12 +131,65 @@ const NoData = styled.div`
   margin-top: 16px;
 `;
 
+const PS3Game = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+const PS3GameName = styled.div`
+  margin-left: 24px;
+`;
+
 /** TODO:
  * split form, table and alert into different components
  * button to clear invalid products
  * actions to remove, archive as acquired
  * filtering: all, discounted, acquired
  */
+
+const PS3list = [
+  {
+    name: 'Resistance: Fall of Man',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP9000-NPEA00430_00-RESISTANCE1PSN01/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'Resistance 2',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP9000-BCES00226_00-DISCONLYPARENT00/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'Resistance 3',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP9000-BCES01118_00-DISCONLYPARENT00/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'inFAMOUS',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP9000-NPEA00252_00-GINFAMOUSX000001/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'inFAMOUS 2',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP9000-NPEA00318_00-GINFAMOUS2000001/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'Alpha Protocol',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP0177-BLES00704_00-DISCONLYPARENT00/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'Binary Domain',
+    imageSrc:
+      'https://store.playstation.com/store/api/chihiro/00_09_000/container/FI/en/99/EP0177-NPEB00915_00-BINARYDOMAINPSN1/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=672&h=672',
+  },
+  {
+    name: 'Tales of Symphonia Chronicles',
+    imageSrc:
+      'https://www.vpd.fi/media/amasty/webp/catalog/product/cache/a0b3a019c90d2db0a2e9ebd40c353a34/p/s/ps3_tales_of_symphonia_chronicles_pegi12_jpg.webp',
+  },
+];
 
 const PlaystationView = () => {
   const {
@@ -389,6 +442,14 @@ const PlaystationView = () => {
           )}
           {isLoading && <LinearProgress />}
         </TableContent>
+
+        <TableTitle>PS3 Wishlist</TableTitle>
+        {PS3list.map((item) => (
+          <PS3Game key={item.name}>
+            <ItemImage src={item.imageSrc} />
+            <PS3GameName>{item.name}</PS3GameName>
+          </PS3Game>
+        ))}
       </Content>
     </Root>
   );
