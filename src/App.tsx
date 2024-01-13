ffea81fa5ade998ed5243390a772b726ef5f3a3f
Fabiano Brito <fabiano.brito@wolt.com>
2024-01-13 15:23:47 +0200

    Back again to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import PortfolioView from './portfolio';
 import Layout from './portfolio/components/Layout';
 import ProjectDetails from './portfolio/components/ProjectDetails';
@@ -20,7 +20,7 @@ const App = () => {
     },
   });
   return (
-    <BrowserRouter>
+    <HashRouter>
       <QueryParamProvider adapter={ReactRouter6Adapter}>
         <Routes>
           <Route path="/" element={<Navigate to="/portfolio" replace />} />
@@ -32,7 +32,7 @@ const App = () => {
           <Route path="*" element={<h2>Nothing to see here... :/</h2>} />
         </Routes>
       </QueryParamProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
