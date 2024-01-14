6d2cdef3c962ad72b2b7efdfd3a68ca589d40251
Fabiano Brito <fabiano.brito@wolt.com>
2024-01-14 23:29:26 +0200

    Remove other unused files and dependency

--- types/root.ts
+++
@@ -1,9 +0,0 @@
-export type Role = 'portfolio_admin' | 'playstation_user';
-
-export type User = {
-  _id?: string;
-  username: string;
-  password: string;
-  roles: Role[];
-  psStoreHash?: string;
-};
